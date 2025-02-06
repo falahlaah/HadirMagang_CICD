a0a525c6d407cd4d4012c860c3bc9b340a5f98c0
falahlaah <muhammadfalahlah@gmail.com>
2025-02-06 16:36:52 +0700

    update target TestRunner Commit

--- src/test/java/com/myCompany/TestRunner.java
+++ src/test/java/com/myCompany/TestRunner.java
@@ -3,7 +3,7 @@ package com.myCompany;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/main/resources/features/02LaporanSemua.feature"},
+@CucumberOptions(features = {"src/main/resources/features"},
         glue = "com.myCompany",
         plugin = {"pretty","html:target/cucumber-report.html","json:target/cucumber.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
